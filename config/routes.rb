e4a40c3ab78bb3567db3b9c53d9b5794440938dc
Yordano Contreras <yordanoc.pro@outlook.com>
2019-05-22 11:31:40 +0200

    pg allready

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
+   #   delete "doses/:id",  to: "doses#destroy", as: :delete
+   delete "cocktails/:id",  to: "cocktails#destroy", as: :destroy
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+   resources :doses, only: [:destroy]
 end
